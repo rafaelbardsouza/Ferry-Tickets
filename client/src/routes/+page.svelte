4c62b8e1b9cf59080f67c29089bca2cc582d10d9
rafaelbardsouza <152352367+rafaelbardsouza@users.noreply.github.com>
2024-08-20 00:48:01 -0300

    create svelte app + base structure

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,49 @@
+<script>
+    import '../styles/main.css'
+    let username = '';
+    let password = '';
+
+</script>
+
+<main>
+    <div class="container">
+        <img src="/favicon.png" alt="Ferry Tickets Logo" class="logo"/>
+        <h1>Welcome to Ferry Tickets App!</h1>
+        <p>First, log-in or create your user.</p>
+        <div class="form-container">
+            <input class="input-field" placeholder="Username" bind:value={username}/>
+            <input class="input-field" type="password" placeholder="Password" bind:value={password}/>
+            <button class="button" on:click={() => {console.log('login', {username, password})}}>Log-in</button>
+            <button class="button" on:click={() => {console.log('signin', {username,password})}}>Sign-in</button>
+            <small class="note">Note: Passwords are not encrypted or protected in this application. Use with caution.</small>
+        </div>
+    </div>
+</main>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=SF+Pro+Text:wght@400;700&display=swap');
+
+    .form-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    .input-field {
+        display: block;
+        margin: 10px auto;
+        padding: 10px;
+        width: 80%;
+        max-width: 300px;
+        border: 1px solid #333;
+        border-radius: 5px;
+        background-color: #2c2c2c;
+        color: #e0e0e0;
+    }
+    .note {
+        font-weight: bold;
+        display: block;
+        margin-top: 20px;
+        color: #cf6679;
+        font-size: 0.9em;
+    }
+</style>
---
+++ client/src/routes/tickets/+page.svelte
@@ -0,0 +1,96 @@
+<script>
+    import  '../../styles/main.css'
+</script>
+
+<main>
+    <div class="page">
+        <div class="container">
+            <img src="/favicon.png" alt="Ferry Tickets Logo" class="logo"/>
+            <h1>Hello, Rafael! 👋</h1>
+            <p>Here are your tickets:</p>
+            <ul class="tickets">
+                <li><a href="#a">🎫 ticket 1</a></li>
+                <li><a href="#a">🎫 ticket 2</a></li>
+                <li><a href="#a">🎫 ticket 3</a></li>
+                <li><a href="#a">🎫 ticket 4</a></li>
+                <li><a href="#a">🎫 ticket 5</a></li>
+                <li><a href="#a">🎫 ticket 6</a></li>
+                <li><a href="#a">🎫 ticket 7</a></li>
+            </ul>
+            <button class="button">Create Ticket</button>
+        </div>
+    </div>
+</main>
+
+<style>
+    .page {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        color: #fff;
+    }
+
+    .container {
+        text-align: center;
+        background: #1e1e1e;
+        padding: 40px;
+        border-radius: 10px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5);
+        width: 100%;
+        max-width: 600px;
+        box-sizing: border-box;
+    }
+
+    .tickets {
+        list-style: none;
+        padding: 0;
+        max-height: 300px;
+        overflow-y: auto;
+    }
+
+    .tickets::-webkit-scrollbar {
+        display: none;
+    }
+
+    .tickets {
+        -ms-overflow-style: none;
+        scrollbar-width: none;
+    }
+
+    .tickets a {
+        width: 300px;
+        height: 30px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 10px;
+        margin: 5px 0;
+        background: navy;
+        color: #fff;
+        text-decoration: none;
+        border-radius: 5px;
+    }
+
+    .tickets a:hover {
+        background-color: #3700b3;
+        transition: transform 0.3s ease-in-out;
+    }
+
+    .button {
+        background-color: #3700b3;
+        color: #fff;
+        font-weight: bold;
+        width: 80%;
+        max-width: 320px;
+        margin: 10px;
+        padding: 10px 20px;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        transition: background-color 0.3s ease;
+    }
+
+    .button:hover {
+        background-color: #6200ea;
+    }
+</style>
